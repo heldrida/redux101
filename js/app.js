84d12876b4f9f8c45e0fd76c725f22e9d8aad88e
Helder <helder@lamoulade.com>
2016-04-21 15:50:07 +0100

    Added the expect library by Michael Jackson for tests or better assertions; Described the Reducer fn;

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Count is a `reducer function`
+// Described as a `pure function`
+// takes the previous state and dispatched action
+// and returns the next state
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -8,6 +12,26 @@ const counter = (state = 0, action) => {
       return state;
   }
 }
+
+// Tests using `expect` by Michael Jackson
+expect(
+	counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+	counter(1, { type: 'DECREMENT' })
+).toEqual(0);
+
+expect(
+	counter(3, { type: 'INCREMENT' })
+).toEqual(4);
+
+expect(
+	counter(undefined, { type: 'OTHER' })
+).toEqual(0);
+
+console.log('Tests passed!');
+
 
 const { createStore } = Redux;
 const store = createStore(counter);
