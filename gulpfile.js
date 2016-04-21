bd5fd3561f48904b8e87e3be1d5dec2670c05d35
Helder <helder@lamoulade.com>
2016-04-21 15:39:20 +0100

    rm notification

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('reload', function () {
 gulp.task('serve', ['watch'], function () {
 
 	browserSync.init({
+        notify: false,
         server: {
             baseDir: "./"
         }
